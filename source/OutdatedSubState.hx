603ff51baf3b83ad2878e0dcb27b867cc6f11ed4
Bonny Wuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2024-11-22 00:38:49 -0300

    Meow

--- source/OutdatedSubState.hx
+++ source/OutdatedSubState.hx
@@ -18,7 +18,7 @@ class OutdatedSubState extends MusicBeatState
 		add(bg);
 		var ver = "v" + Application.current.meta.get('version');
 		var txt:FlxText = new FlxText(0, 0, FlxG.width,
-			"WARNING!!! \n\n\n\nYou are using a early version of ProjectFNF!! \n Some Features might not work as they should, And crashes will happen. \n Press Enter to go to the Latest Stable Release,\n or press ESC to continue.",
+			"WARNING!!! \n\n\n\nYou are using a early version of ProjectFNF!! \n Some Features might not work as they should, And crashes will happen. \n Press Enter to go to the Latest Stable Release,\n or press ESC to continue.\n Port by GaboWuz\n GaboWuz: Hiiiii",
 			32);
 		txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		txt.screenCenter();
@@ -27,11 +27,19 @@ class OutdatedSubState extends MusicBeatState
 
 	override function update(elapsed:Float)
 	{
-		if (controls.ACCEPT)
+		#if mobile
+        var jusTouched:Bool = false;
+
+        for (touch in FlxG.touches.list)
+          if (touch.justPressed)
+            jusTouched = true;
+        #end
+	
+		if (controls.ACCEPT #if android || FlxG.android.justReleased.BACK #end)
 		{
 			FlxG.openURL("https://github.com/aflacc/ProjectFNF/releases/tag/v0.4-a");
 		}
-		if (controls.BACK)
+		if (controls.BACK #if mobile || jusTouched #end)
 		{
 			leftState = true;
 			FlxG.switchState(new MainMenuState());
